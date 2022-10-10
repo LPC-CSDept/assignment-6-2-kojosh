86d9f7f78955b3bd11215359825e6d01999adf7a
kojosh <111550404+kojosh@users.noreply.github.com>
2022-10-09 23:40:50 -0700

    header file for assignment 6-2

--- A62.hpp
+++ A62.hpp
@@ -7,13 +7,32 @@ using namespace std;
 // complete two functions
 double nthpowerp(int n)
 {
+    int nthPow = 1;
         // return n_th power number
+        int num = n;
+        // For loop to get nthPow
+        for ( int i = 0; i < num; i++ ) {
+            nthPow *= 2;
+        }
+        return nthPow;
 }
+
 double nthpowern(int n)
 {
+    double iversePow;
+    int nthPow = 1;
         // return n_th power number
+        int num = n * -1;
+        // For loop to get nthPow
+        for ( int i = 0; i < num; i++ ) {
+            nthPow *= 2;
+        }
+        double inversePow = 1.0 / nthPow;
+        return inversePow;
 }
+
 void printout(double result)
 {
+        
         cout << "The result is " << setw(10) << fixed << setprecision(5) << result << endl;
 }
